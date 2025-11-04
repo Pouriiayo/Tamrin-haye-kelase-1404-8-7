f5742d4ba201271df0743af39a841abde2c0c068
pouria yousefi daneshjoo <164668228+Pouriiayo@users.noreply.github.com>
2025-11-04 21:39:09 +0330

    Add files via upload

---
+++ Tamrin 6/Tamrin 6/Program.cs
@@ -0,0 +1,38 @@
+﻿float sumAV = 0;
+float sumV = 0;
+while (true)
+{
+    float number;
+    float Vazn;
+    Console.WriteLine("adad vared konid :");
+    string input = Console.ReadLine();
+    if (input.ToLower() == "q")
+    {
+        break;
+    }
+    number = float.Parse(input);
+
+    Console.WriteLine("Vazne adad ra vared konid :");
+    string inputV = Console.ReadLine();
+
+    if (inputV.ToLower() == "q")
+    {
+        break;
+    }
+
+    Vazn = float.Parse(inputV);
+
+
+    sumAV += (number * Vazn);
+    sumV += Vazn;
+}
+float AverageBaVazn = sumAV / sumV;
+
+if (AverageBaVazn != 0)
+{
+    Console.WriteLine($"miangin barabar ast ba = {AverageBaVazn}");
+}
+else
+{
+    Console.WriteLine("nashod");
+}
